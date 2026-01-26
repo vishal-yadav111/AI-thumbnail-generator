823a774ec88b775697ec08da6b65f7630cc2f07f
Vishal yadav <vidhalyadav6969@gmail.com>
2026-01-27 03:03:06 +0530

    added branch

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { SoftBackdrop } from './Login/SoftBackdrop'
+import { useParams } from 'react-router-dom';
 
 export const Generate = () => {
+    const {id}=useParams();
+    const [loading, setLoading] = React.useState(false);
+    const [thumbnails, setThumbnails] = React.useState(false);
+    const [title, setTitle] = React.useState(" ");
+    const [additionaldetails, setAdditionalDetails] = React.useState(" ");
   return (
-    <div>Generate</div>
+    <>
+    <SoftBackdrop/>
+    </>
   )
 }
